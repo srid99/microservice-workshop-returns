306d3321962be0f6708df6e92af73949c1a12566
srid99 <srid99@gmail.com>
2016-05-19 20:46:56 +0200

    Hide implementation

--- src/main/java/in/srid/microservices/returns/resources/ReturnsResource.java
+++ src/main/java/in/srid/microservices/returns/resources/ReturnsResource.java
@@ -1,39 +1,31 @@
 package in.srid.microservices.returns.resources;
 
-import java.util.List;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
-import in.srid.microservices.returns.api.Returns;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.codahale.metrics.annotation.Timed;
-import com.netflix.loadbalancer.Server;
-import com.smoketurner.dropwizard.consul.ribbon.RibbonJerseyClient;
+
+import in.srid.microservices.returns.api.Returns;
 
 @Path("/returns")
 @Produces(MediaType.APPLICATION_JSON)
 public class ReturnsResource {
     private static final Logger LOG = LoggerFactory.getLogger(ReturnsResource.class);
 
-    private final RibbonJerseyClient client;
+    private final Client client;
 
-    public ReturnsResource(RibbonJerseyClient client) {
+    public ReturnsResource(Client client) {
         this.client = client;
-    }
-
-    @GET
-    @Timed
-    public List<Server> getAvailableServers() {
-        return client.getAvailableServers();
     }
 
     @GET
